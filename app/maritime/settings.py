d495109448076f92752250b03edaa08cdbb6cd26
Zeeshi <zeeshanmdsol@gmail.com>
2023-12-07 00:59:03 +0500

    github actions config with docker hub

--- app/maritime/settings.py
+++ app/maritime/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_apis',
     'core'
 ]
 
@@ -123,8 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-AUTH_USER_MODEL = 'rest_apis.UserProfile'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
