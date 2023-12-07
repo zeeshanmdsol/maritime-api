cc8d397639bbdc5bfecc147ecc8539adb1792809
Zeeshi <zeeshanmdsol@gmail.com>
2023-12-08 01:17:44 +0500

    integrated swagger api in the project

--- app/maritime/settings.py
+++ app/maritime/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'core'
+    'core',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
